180e3f2385b221633ed422f690c4b6f6faa463d9
Nthabeleng Molapo <nthabelengmolapo07@gmail.com>
2023-07-13 15:53:20 -0700

    5-printf.c

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
- * main-this will print required outcome
- * Return: 0 always
+ * main - the main will print with proper grammar,but the outcome is a piece of art
+ *
+ * Return: 0 (succesful)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
