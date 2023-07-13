9a614951f0f2c9cc2f8848e0e77922c4554a98fc
Nthabeleng Molapo <nthabelengmolapo07@gmail.com>
2023-07-13 16:59:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Enter required info
+ * Return: 0(success)
+ */
+int main(void)
+{
+	printf("Size of a char:%lu byte(s)").sizeof(char);
+	printf("Size of a int:%lu byte(s)").sizeof(int);
+	printf("Size of a long int:%lu byte(s)").sizeof(long int);
+	printf("Size of a long long:%lu byte(s)").sizeof(long long);
+	printf("Size of a float:%lu byte(s)").sizeof(float);
+	return (0);
