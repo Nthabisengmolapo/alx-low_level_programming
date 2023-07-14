bd5fec733c7b544e247229ca8e7b6adb9544aadf
Nthabeleng Molapo <nthabelengmolapo07@gmail.com>
2023-07-13 17:33:19 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - prints"Programming is like building a multilingual puzzle" 
+ * main - Entered info to print 
  * Return: 0(success)
  *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle\n"");
 	return (0);
 }
